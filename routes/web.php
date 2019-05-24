3e1c6d54465ac8ab67e0805d05d3be1fc5ff08c8
brunoestevao@onsystem.pt <bruno@onsystem.local>
2019-05-24 13:49:30 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('layouts.default');
 });
+	Route::resource('clients','ClientController');
+	//Route::get('devices/{device}/upload','DeviceController@upload')->name('devices.upload');
+	Route::post('devices/{device}/upload','DeviceController@upload')->name('devices.upload');
+	//Route::get('backups/{id}/download','DeviceController@download')->name('devices.download');
+	Route::get('backups/download','BackupController@download')->name('backups.download');
+	//Route::get('backups/download','BackupController@download')->name('backup.download');
+	Route::resource('backups','BackupController');
+	Route::resource('devices','DeviceController');
+	Route::resource('suppliers','SupplierController');
+	Route::resource('accounts','AccountController');
+	Route::resource('licenses','LicenseController');
+	Route::resource('items','ItemController');
+	Route::resource('orders','OrderController');
